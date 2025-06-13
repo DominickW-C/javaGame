909bd8eea290ead09104d231baed8aa879b9f839
DominickW-C <dwcouture04@gmail.com>
2025-06-13 20:22:00 +0000

    adjsuted screen size, position, and added JPanel

--- app.java
+++ app.java
@@ -1,18 +1,59 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 public class app {
+
     public static void main (String[] args) {
-        System.out.println("test");
-        JFrame frame = new JFrame("test");
+    
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel(); 
+        Window win = new Window(frame, panel);
+        win.run();
+
+        JLabel txt = new JLabel();
+        txt.setText("test");
+        panel.add(txt);
+
+    }
+}
+
+class Window {
+
+    JFrame frame;
+    JPanel panel;
+
+    Window (JFrame frame, JPanel panel) {
+        this.frame = frame;
+        this.panel = panel;
+    }
+
+    /**
+     * Creates a window with frame and panel given in init.
+     */
+    public void run() {
+
+        //finds the screensize so that the window can be placed in the middle of screen
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); 
+        double width = screenSize.getWidth();
+        double height = screenSize.getHeight();
+
+        //sizes frame, default to sys look and feel
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        frame.setSize(640, 480);
 
         //causes the program to close when the frame closes
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocation(((int)width/2)-320, ((int)height/2)-240);
         
-        //should set the frame size to whatever is inside
-        //frame.pack();
-
         //makes the frame actually visible
         frame.setVisible(true);
-        
-    }
+
+        //sizes panel to frame 
+        panel.setSize(640, 480);
+
+        //add panel to frame
+        frame.add(panel);
+    } 
+
 }
--- notes.md
+++ notes.md
@@ -1,4 +1,10 @@
 ** 6-7-25 **
-- the initial commit for this repo not a lot was done besides starting it. I have a blank screen appearing
+- The initial commit for this repo not a lot was done besides starting it. I have a blank screen appearing
   and that is about it. I currently don't have much of an idea for the game, probably going to do some 2D
   platformer because it seems like a good start.
+
+** 6-13-25 **
+- Wow I really need to work on this more. Today I was able to make it so that the window size is 640x480
+  as I feel like that is a good size to start with. I also figured out how to make it so the window will
+  always make itself in the middle of the screen. Lastly, I added a main panel along with a label in order
+  to add text to the screen. The next few things planned will be event listeners and refreshing the screen.
